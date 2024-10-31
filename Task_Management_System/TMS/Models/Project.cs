883dcdba7f75841fd54a3811d72b8516fd0db78d
KMJonas <joaocosta.pessoal@gmail.com>
2024-10-31 08:48:16 +0000

    ProjectController endpoints feitos!

--- Task_Management_System/TMS/Models/Project.cs
+++ Task_Management_System/TMS/Models/Project.cs
@@ -1,7 +1,11 @@
 namespace TMS.Models;
 
 public class Project
-{ 
-        public int Id { get; set; }
-        public string DesignationProject { get; set; }
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
 }
