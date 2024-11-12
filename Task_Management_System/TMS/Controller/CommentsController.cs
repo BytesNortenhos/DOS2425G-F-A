5bc8d26b21926b2fbef271d1e9ceaed50e858367
Dinis Faryna <1231530@isep.ipp.pt>
2024-11-12 21:44:25 +0000

    Сomentários associados a utilizadores

--- Task_Management_System/TMS/Controller/CommentsController.cs
+++ Task_Management_System/TMS/Controller/CommentsController.cs
@@ -9,11 +9,11 @@ public class CommentsController : ControllerBase
 {
     private static List<Comments> comments = new List<Comments>
     {
-        new Comments { Id = 1, Text = "Aaa", TaskId = new TaskItem { Id = 1, TicketNumber = "001", Title = "Tarefa 1", Description = "Aaa", IsCompleted = false, DueDate = new DateTime(2023, 10, 5) }},
-        new Comments { Id = 2, Text = "Bbb", TaskId = new TaskItem { Id = 2, TicketNumber = "002", Title = "Tarefa 2", Description = "Bbb", IsCompleted = false, DueDate = new DateTime(2023, 10, 6) }},
-        new Comments { Id = 3, Text = "Ccc", TaskId = new TaskItem { Id = 3, TicketNumber = "003", Title = "Tarefa 3", Description = "Ccc", IsCompleted = false, DueDate = new DateTime(2023, 10, 7) }},
-        new Comments { Id = 4, Text = "Ddd", TaskId = new TaskItem { Id = 4, TicketNumber = "004", Title = "Tarefa 4", Description = "Ddd", IsCompleted = false, DueDate = new DateTime(2023, 10, 8) }},
-        new Comments { Id = 5, Text = "Eee", TaskId = new TaskItem { Id = 5, TicketNumber = "005", Title = "Tarefa 5", Description = "Eee", IsCompleted = false, DueDate = new DateTime(2023, 10, 9) }},
+        new Comments { Id = 1, Text = "Aaa", User = new User {Id= 1,DesignationUser = "User 1"}, TaskId = new TaskItem { Id = 1, TicketNumber = "001", Title = "Tarefa 1", Description = "Aaa", IsCompleted = false, DueDate = new DateTime(2023, 10, 5) }},
+        new Comments { Id = 2, Text = "Bbb", User = new User {Id= 2,DesignationUser = "User 2"}, TaskId = new TaskItem { Id = 2, TicketNumber = "002", Title = "Tarefa 2", Description = "Bbb", IsCompleted = false, DueDate = new DateTime(2023, 10, 6) }},
+        new Comments { Id = 3, Text = "Ccc", User = new User {Id= 3,DesignationUser = "User 3"}, TaskId = new TaskItem { Id = 3, TicketNumber = "003", Title = "Tarefa 3", Description = "Ccc", IsCompleted = false, DueDate = new DateTime(2023, 10, 7) }},
+        new Comments { Id = 4, Text = "Ddd", User = new User {Id= 4,DesignationUser = "User 4"}, TaskId = new TaskItem { Id = 4, TicketNumber = "004", Title = "Tarefa 4", Description = "Ddd", IsCompleted = false, DueDate = new DateTime(2023, 10, 8) }},
+        new Comments { Id = 5, Text = "Eee", User = new User {Id= 5,DesignationUser = "User 5"}, TaskId = new TaskItem { Id = 5, TicketNumber = "005", Title = "Tarefa 5", Description = "Eee", IsCompleted = false, DueDate = new DateTime(2023, 10, 9) }},
     };
 
     [HttpGet]
--- Task_Management_System/TMS/Models/Comments.cs
+++ Task_Management_System/TMS/Models/Comments.cs
@@ -2,7 +2,8 @@ namespace TMS.Models;
 
 public class Comments
 { 
-        public int Id { get; set; }
-        public string Text { get; set; }
-        public TaskItem TaskId { get; set; }
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public User User { get; set; }
+    public TaskItem TaskId { get; set; }
 }
